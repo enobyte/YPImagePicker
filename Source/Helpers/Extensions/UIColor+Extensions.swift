72ca01c246b714f962ebb49b3d39495ee1b69a99
Ferico Samuel <ferico55@gmail.com>
2022-10-03 23:05:57 +0700

    public modifier

--- Source/Helpers/Extensions/UIColor+Extensions.swift
+++ Source/Helpers/Extensions/UIColor+Extensions.swift
@@ -13,7 +13,7 @@ extension UIColor {
         self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
     }
 
-    static var offWhiteOrBlack: UIColor {
+    static public var offWhiteOrBlack: UIColor {
         if #available(iOS 13, *) {
             return UIColor { (traitCollection: UITraitCollection) -> UIColor in
                 let rgbValue: CGFloat = traitCollection.userInterfaceStyle == .dark ? 0 : 247
@@ -27,14 +27,14 @@ extension UIColor {
     /// The color for text labels that contain primary content.
     ///
     /// Like `.label`, but backwards-compatible with iOS 12 and lower.
-    static var ypLabel: UIColor {
+    static public var ypLabel: UIColor {
         if #available(iOS 13, *) {
             return .label
         }
         return .black
     }
     
-    static var ypSecondaryLabel: UIColor {
+    static public var ypSecondaryLabel: UIColor {
         if #available(iOS 13, *) {
             return .secondaryLabel
         }
@@ -44,7 +44,7 @@ extension UIColor {
     /// The color for content layered on top of the main background.
     ///
     /// Like `.secondarySystemBackground`, but backwards-compatible with iOS 12 and lower.
-    static var ypSecondarySystemBackground: UIColor {
+    static public var ypSecondarySystemBackground: UIColor {
         if #available(iOS 13, *) {
             return .secondarySystemBackground
         }
@@ -54,7 +54,7 @@ extension UIColor {
     /// The color for the main background of your interface.
     ///
     /// Like `.systemBackground`, but backwards-compatible with iOS 12 and lower.
-    static var ypSystemBackground: UIColor {
+    static public var ypSystemBackground: UIColor {
         if #available(iOS 13, *) {
             return .systemBackground
         }
@@ -64,7 +64,7 @@ extension UIColor {
     /// The base blue color.
     ///
     /// Like `.systemBlue`, but backwards-compatible with iOS 12 and lower.
-    static var ypSystemBlue: UIColor {
+    static public var ypSystemBlue: UIColor {
         if #available(iOS 13, *) {
             return .systemBlue
         }
@@ -74,7 +74,7 @@ extension UIColor {
     /// The base gray color.
     ///
     /// Like `.systemGray`, but backwards-compatible with iOS 12 and lower.
-    static var ypSystemGray: UIColor {
+    static public var ypSystemGray: UIColor {
         if #available(iOS 13, *) {
             return .systemGray
         }
@@ -84,7 +84,7 @@ extension UIColor {
     /// The color for red, compatible with dark mode in iOS 13.
     ///
     /// Like `.red`, but backwards-compatible with iOS 12 and lower.
-    static var ypSystemRed: UIColor {
+    static public var ypSystemRed: UIColor {
         if #available(iOS 13, *) {
             return .systemRed
         }
